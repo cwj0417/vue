c48c367b7a04bcd81cbde0ae8565de5196f01841
chenwj <731028571@qq.com>
2018-03-21 15:47:37 +0800

    chore: commit

--- src/core/index.js
+++ src/core/index.js
@@ -3,6 +3,7 @@ import { initGlobalAPI } from './global-api/index'
 import { isServerRendering } from 'core/util/env'
 import { FunctionalRenderContext } from 'core/vdom/create-functional-component'
 
+// 核心入口
 initGlobalAPI(Vue)
 
 Object.defineProperty(Vue.prototype, '$isServer', {
